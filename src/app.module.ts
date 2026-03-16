46d33e1414c2a73ae0bc4980d00c173a0cb3b89d
Apoorv Gupta <apoorvcurry@gmail.com>
2026-03-16 17:10:59 +0530

    feat: finalize ExpensesController and prepare for testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { ExpensesService } from './expenses/expenses.service';
 import { ExpensesController } from './expenses/expenses.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController, ExpensesController],
   providers: [AppService, PrismaService, ExpensesService],
 })
-export class AppModule {}
+export class AppModule { }
