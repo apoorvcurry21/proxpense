b1346f666d20c33e93ac796fa4519e19433339eb
Apoorv Gupta <apoorvcurry@gmail.com>
2026-03-16 12:29:33 +0530

    feat: implement createExpense logic in ExpensesService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { ExpensesService } from './expenses/expenses.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, ExpensesService],
 })
 export class AppModule {}
---
+++ src/expenses/expenses.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ExpensesService {
+  constructor(private prisma: PrismaService) {}
+
+  async createExpense(amount: number, category: string, userId: string) {
+    return this.prisma.expense.create({
+      data: {
+        amount,
+        category,
+        userId,
+      },
+    });
+  }
+}
